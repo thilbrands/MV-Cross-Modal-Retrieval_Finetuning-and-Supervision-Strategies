9721a3d1cb5ff0339dfb2d121c93109e0e802884
thilbran <theo_paul.hilbrands@smail.th-koeln.de>
2026-04-27 17:37:54 +0200

    Update evaluation script to extend job time limit from 1 hour to 4 hours for improved processing capacity.

--- jobs/evaluation.sh
+++ jobs/evaluation.sh
@@ -12,7 +12,7 @@
 #SBATCH --partition=paula
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=24GB
-#SBATCH --time=0-1:00:00
+#SBATCH --time=0-4:00:00
 
 #SBATCH --output=/work2/ra39oxet-DatasetAudioSetSubset/logs/evaluation_%j.out
 #SBATCH --error=/work2/ra39oxet-DatasetAudioSetSubset/logs/evaluation_%j.err
